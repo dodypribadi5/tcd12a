68a802539efa02b33310cc40f5ee369e48720fe0
dodypribadi5 <dodypribadi5@gmail.com>
2025-06-30 09:49:00 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 let tglNow = new Date();
-let waktuBatas = new Date("2025-08-05T23:59:59");
+let waktuBatas = new Date("2025-08-20T23:59:59");
 if (tglNow.getTime() >= waktuBatas.getTime()) {
   document.location.href = 'https://suspend-web.pages.dev';
 } else {
